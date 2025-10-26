ae8329da60c162114435d555b72a2543881d89ea
MarianaZapata3 <marianagabrielazapata@gmail.com>
2025-10-25 22:06:32 -0300

    modificado

--- TrabajoPractico_2/proyecto_3/modules/ordenamientoquicksort.py
+++
@@ -1,7 +0,0 @@
-def quicksort(lista):
-    if len(lista) <= 1:
-        return lista
-    pivote = lista[0]
-    menores = [x for x in lista[1:] if x <= pivote]
-    mayores = [x for x in lista[1:] if x > pivote]
-    return quicksort(menores) + [pivote] + quicksort(mayores)
